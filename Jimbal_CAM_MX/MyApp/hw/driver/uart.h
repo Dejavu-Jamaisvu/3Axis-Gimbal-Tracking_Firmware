01a62d143bce4c3cd0239f3065064e74a223d262
hsol0212 <hyesol0212@gmail.com>
2026-04-28 16:42:46 +0900

    feat: lcd에 빨간 점을 실시간으로 띄우기 위한 과정들

--- Jimbal_CAM_MX/MyApp/hw/driver/uart.h
+++ Jimbal_CAM_MX/MyApp/hw/driver/uart.h
@@ -1,7 +1,15 @@
-#ifndef __HW_DRIVER_UART_H_ 
-#define __HW_DRIVER_UART_H_
+#pragma once
+#include <stdint.h>
+#include <stdbool.h>
+#include "cmsis_os.h"
+#include "stm32f4xx_hal.h"
 
-#include "hw_def.h"
+// ESP32에서 받은 좌표 데이터
+typedef struct {
+    int  cx;
+    int  cy;
+    bool detected;
+} TrackData;
 
 bool     uartInit(void);
 bool     uartOpen(uint8_t ch, uint32_t baudrate);
@@ -9,4 +17,5 @@ uint32_t uartAvailable(uint8_t ch);
 uint8_t  uartRead(uint8_t ch);
 uint32_t uartWrite(uint8_t ch, uint8_t *p_data, uint32_t len);
 
-#endif
+void      uartParsePacket(void);   // 매 루프마다 호출
+TrackData uartGetTrackData(void);  // 최신 좌표 반환
